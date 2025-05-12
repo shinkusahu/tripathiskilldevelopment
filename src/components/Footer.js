f77c91ded6eb84fe395281178604634d692a0daf
abhishek tiwari <tiwariabhishek0602@gmail.com>
2025-05-12 14:32:59 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,17 @@ const Footer = () => {
     <footer className="footer">
       <div className="container">
         <p className="footer-text">
-          Created by <span className="highlight">Abhishek Tiwari</span> | Maintained by <span className="highlight">Techno Tech Company</span>
+          Created by{' '}
+          <a 
+            href="https://www.linkedin.com/in/abhishek-tiwari-3567b465" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="linkedin-link"
+          >
+            <span className="highlight">Abhishek Tiwari</span>
+          </a>{' '}
+          | Maintained by{' '}
+          <span className="highlight">Techno Tech Company</span>
         </p>
       </div>
     </footer>
